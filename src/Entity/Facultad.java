a49acd6204045d0c3954c940e7cf04ab9c28196c
eli-hu-24 <eliasHuaringa244@gmail.com>
2026-07-13 22:24:21 -0500

    implementacion de la mayoria de los paquetes  del proyecto

--- src/Entity/Facultad.java
+++ src/Entity/Facultad.java
@@ -4,10 +4,51 @@
  */
 package Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author USUARIO
  */
 public class Facultad {
-    
+    private int id;
+    private String nombre;
+
+    // Relación lógica con Carrera
+    private List<Carrera> carreras;
+
+    public Facultad() {
+        carreras = new ArrayList<>();
+    }
+
+    public Facultad(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+        this.carreras = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Carrera> getCarreras() {
+        return carreras;
+    }
+
+    public void agregarCarrera(Carrera carrera) {
+        carreras.add(carrera);
+    }
 }
